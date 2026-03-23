e396d4984592aca264ba2ecb7a2df923a5ae3eb1
iwakunai <13847634+iwakunai@users.noreply.github.com>
2026-03-23 21:06:07 +0800

    Added error handling to endpoints

--- services/payment-service/app/api/payment_routes.py
+++ services/payment-service/app/api/payment_routes.py
@@ -1,29 +1,36 @@
-from termios import ECHOE
-
 from sqlalchemy import insert
 
 from app import db
 from app.models import Payment, PaymentStatus
-from flask import Blueprint, jsonify, current_app
+from flask import Blueprint, jsonify, current_app, request
 
 import logging
 
 logger = logging.getLogger("payment-service")
 api = Blueprint("api", __name__)
 
+@api.errorhandler(Exception)
+def handle_exceptions(e):
+    return jsonify(error=str(e)), e.response.status_code
 
-# Clicking make payment on site creates Payment Intent using order information
-# TODO replace order id with payment class
+
+# TODO replace order id with order class?
 @api.route('/checkout', methods=["POST"])
 def create_payment_intent():
     """
-    Create payment intent
+    Create payment intent after order created
+    Order information passed from Make Order Saga
     """
+    data = request.get_json()
+    logger.info(data)
+
+    amount_cents = data["amount"] * 100 # Stripe takes amounts in cents...
+
     try:
         stripe_client = current_app.stripe_client
-        intent = stripe_client.v1.payment_intents.create({"amount": 10099, "currency": "sgd"})
+        intent = stripe_client.v1.payment_intents.create({"amount": amount_cents, "currency": "sgd"})
 
-        logger.debug(intent.client_secret)
+        # logger.debug(intent.client_secret)
         return jsonify({"code" :200,
                         "client_secret": intent.client_secret
                         }), 200
