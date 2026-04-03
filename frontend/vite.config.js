53156533f5533894ff6807d22de8e529ffe5986d
ch-costan <ch.costan.2024@smu.edu.sg>
2026-04-03 13:46:58 +0800

    changed env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vite.dev/config/
 export default defineConfig({
-  envDir: '..',
+  envDir: '../frontend',
   plugins: [
     vue(),
     vueDevTools(),
